2189ac0e10a6863e8ea4b425089ad7fa3910ce60
orzechdev <tarasiukdawid@gmail.com>
2018-05-04 22:16:18 +0200

    initial commit

---
+++ app/src/main/java/com/raspberryopener/ActivityMain.java
@@ -0,0 +1,25 @@
+package com.raspberryopener;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ActivityMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button settingsButton = findViewById(R.id.buttonSettings);
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ActivityMain.this, ActivitySettings.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
