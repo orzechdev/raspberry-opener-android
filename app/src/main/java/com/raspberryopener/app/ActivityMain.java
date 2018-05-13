68e72e9a18fccf9c8b7520ecd4899d970c841392
orzechdev <tarasiukdawid@gmail.com>
2018-05-14 00:29:21 +0200

    Turning on/off bluetooth automatically

--- app/src/main/java/com/raspberryopener/app/ActivityMain.java
+++ app/src/main/java/com/raspberryopener/app/ActivityMain.java
@@ -1,11 +1,11 @@
 package com.raspberryopener.app;
 
-import android.app.Activity;
 import android.app.ActivityManager;
 import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
-import android.support.design.widget.Snackbar;
+import android.os.PowerManager;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,6 +17,9 @@ import android.widget.TextView;
 public class ActivityMain extends AppCompatActivity {
 
     private static final int REQUEST_ENABLE_BT = 1;
+    private static boolean bluetoothEnabledByApplication = false;
+
+    private static boolean isRotating = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +27,37 @@ public class ActivityMain extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setupButtons();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
 
         turnOnBluetooth();
+
+        if(isRotating)
+            isRotating = false;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        boolean isScreenOn = true;
+        if(pm != null) {
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP)
+                isScreenOn = pm.isScreenOn();
+            else
+                isScreenOn = pm.isInteractive();
+        }
+
+        if(isChangingConfigurations() && isScreenOn){
+            isRotating = true;
+        }else{
+            isRotating = false;
+            turnOffBluetooth();
+        }
     }
 
     private void setupButtons(){
@@ -41,27 +73,30 @@ public class ActivityMain extends AppCompatActivity {
 
     private void turnOnBluetooth(){
         BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        String snackbarInfo;
-        boolean bluetoothIsOn = false;
+        String connectionInfo;
+        boolean bluetoothIsOn;
         if (mBluetoothAdapter == null) {
             // Device doesn't support Bluetooth
-            snackbarInfo = getResources().getString(R.string.bluetooth_not_supported);
+            connectionInfo = getResources().getString(R.string.bluetooth_not_supported);
             bluetoothIsOn = false;
         }else{
-            // Device support Bluetooth
-            if (!mBluetoothAdapter.isEnabled()) {
-//                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-//                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-                // We do not ask user to turn on bluetooth, we will turn it on without user interaction (without dialog)
-                mBluetoothAdapter.enable();
+            if(!isRotating) {
+                // Device support Bluetooth
+                if (!mBluetoothAdapter.isEnabled()) {
+                    // We do not ask user to turn on bluetooth, we will turn it on without user interaction (without dialog)
+                    mBluetoothAdapter.enable();
+                    bluetoothEnabledByApplication = true;
+                } else {
+                    bluetoothEnabledByApplication = false;
+                }
             }
-            snackbarInfo = getResources().getString(R.string.bluetooth_on);
+            connectionInfo = getResources().getString(R.string.bluetooth_on);
             bluetoothIsOn = true;
         }
         // Showing information
         View parentLayout = findViewById(R.id.parent_layout);
         TextView connectionTextView = parentLayout.findViewById(R.id.textConnection);
-        connectionTextView.setText(snackbarInfo);
+        connectionTextView.setText(connectionInfo);
         if(bluetoothIsOn) {
             parentLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.colorPrimary));
             connectionTextView.setBackgroundColor(ContextCompat.getColor(this, R.color.colorPrimary));
@@ -71,15 +106,38 @@ public class ActivityMain extends AppCompatActivity {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                 window.setStatusBarColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
                 ActivityManager.TaskDescription taskDescription = new ActivityManager.TaskDescription(null, null, ContextCompat.getColor(this, R.color.colorPrimary));
-                ((Activity)this).setTaskDescription(taskDescription);
+                (this).setTaskDescription(taskDescription);
             }
         }
+    }
 
-//            Snackbar snackbar = Snackbar.make(parentLayout, snackbarInfo, Snackbar.LENGTH_LONG);
-//            View sbView = snackbar.getView();
-//            sbView.setBackgroundColor(ContextCompat.getColor(this.getApplicationContext(), R.color.snackbar_background));
-//            TextView textView = (TextView) sbView.findViewById(android.support.design.R.id.snackbar_text);
-//            textView.setBackgroundColor(ContextCompat.getColor(this.getApplicationContext(), R.color.snackbar_background));
-//            snackbar.show();
+    private void turnOffBluetooth(){
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        String connectionInfo = "";
+        boolean bluetoothIsOff;
+        if (mBluetoothAdapter != null && bluetoothEnabledByApplication && mBluetoothAdapter.isEnabled()) {
+            // Turn off bluetooth
+            mBluetoothAdapter.disable();
+            connectionInfo = getResources().getString(R.string.bluetooth_off);
+            bluetoothIsOff = true;
+        }else{
+            bluetoothIsOff = false;
+        }
+        // Showing information
+        if(bluetoothIsOff) {
+            View parentLayout = findViewById(R.id.parent_layout);
+            TextView connectionTextView = parentLayout.findViewById(R.id.textConnection);
+            connectionTextView.setText(connectionInfo);
+            parentLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.colorPrimaryBlack));
+            connectionTextView.setBackgroundColor(ContextCompat.getColor(this, R.color.colorPrimaryBlack));
+            parentLayout.findViewById(R.id.buttonsContainer).setBackgroundColor(ContextCompat.getColor(this, R.color.colorPrimaryBlack));
+            parentLayout.findViewById(R.id.buttonsContainerLinear).setBackgroundColor(ContextCompat.getColor(this, R.color.colorPrimaryBlack));
+            Window window = this.getWindow();
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                window.setStatusBarColor(ContextCompat.getColor(this, R.color.colorPrimaryBlackDark));
+                ActivityManager.TaskDescription taskDescription = new ActivityManager.TaskDescription(null, null, ContextCompat.getColor(this, R.color.colorPrimaryBlack));
+                (this).setTaskDescription(taskDescription);
+            }
+        }
     }
 }
